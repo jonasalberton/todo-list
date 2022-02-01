eca2cb25d56a57adc085634072098565fe62e78a
jonasalberton <jonasceolin@gmail.com>
2022-02-01 16:49:35 -0300

    feat: state

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,27 @@
 import { KeyboardEvent } from "react";
+import { Todo } from "../models/Todo";
 
-function Input() {
+type Props = {
+  onAddNew: (todo: Todo) => void
+}
+
+function Input(props: Props) {
   const enterKey = 'Enter';
 
-  function handleKeyPress(event: KeyboardEvent) {
+  function handleKeyPress(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key !== enterKey) return;
 
-    console.log('Key is Enter');
-    
-
-    
+    const value = event.currentTarget.value;
+    props.onAddNew({id: 12, isCompleted: false, text: value});
   }
   
   return (
     <div>
-
-       <input onKeyPress={handleKeyPress} name="teste" type="text"></input>
+       <input
+        onKeyPress={handleKeyPress}
+        name="teste"
+        type="text">
+      </input>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { Todo } from "../models/Todo";
 import Input from "./Input";
 
 function TodoList() {
-  const [todoList, setTodoList] = useState([1,2,3,4,5,6,7,8,9]);
+  const [todoList, setTodoList] = useState<Todo[]>([]);
 
- 
+  function addTodo(todo: Todo) {
+    setTodoList([...todoList, todo])
+  }
 
   return (
     <div>
-      <Input></Input>
-      { todoList.map((item) => <div key={item}> item {item} </div>)}
+      <Input onAddNew={addTodo}></Input>
+      { todoList.map((item) => <div key={item.id}> item {item.text} </div>)}
     </div>
   );
 }
